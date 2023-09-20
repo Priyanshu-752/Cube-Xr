cd578c1b4af706c6616ea8da83abdd16617cfd01
PRIYANSHU NEGI <priyanshunegi752@gmail.com>
2023-09-20 22:33:34 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import CubeContainer from './components/cube/CubeContainer'
 import XrCubeContainer from './components/xr-cube/XrCubeContainer'
 function App() {
   return (
   <Routes>
-    <Route path="/" element={<CubeContainer />} />
-    <Route path="/cube" element={<CubeContainer />} />
-    <Route path="/xr-cube" element={<XrCubeContainer />} />
+    <Route path="/" element={<XrCubeContainer />} />
   </Routes>
   )
 }
